f607eea4dcc5da8d5cb40ce81417284f472ce046
Pedro Henrique Bizzarri <pedrobizzarri@gmail.com>
2025-01-21 15:36:51 -0300

    Criando mensagem explicativa ao rodar o projeto

--- src/main/java/br/com/maketplace/rabbitmq/integration/MarketplaceRabbitmqIntegrationApplication.java
+++ src/main/java/br/com/maketplace/rabbitmq/integration/MarketplaceRabbitmqIntegrationApplication.java
@@ -8,6 +8,33 @@ public class MarketplaceRabbitmqIntegrationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MarketplaceRabbitmqIntegrationApplication.class, args);
+		System.out.println(
+			"\n------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------" +
+			"\nO sistema foi iniciado... port: 8080" +
+			"\nBem vindo ao projeto Marketplace Rabbit MQ Api" + 
+			"\nO objetivo deste projeto é implementar um sistema de integração utilizando o RabbitMQ, uma ferramenta de mensageria amplamente utilizada para comunicação entre sistemas e troca de informações de maneira assíncrona e escalável." + 
+			"\nAcesse as funções do projeto de chamadas para as urls:  " + 
+			"\n    1. \\pedido\\finalizar - para finalizar um pedido e gerar um pagamento." + 
+			"\n    2. \\pagamento\\listar - para listar todos os pagamentos registrados." +
+			"\n    3. \\usuario\\listar - para listar todos os usuarios registrados." +
+			"\n    4. \\produto\\listar - para listar todos os produtos registrados." + 
+			"\nO json de pagamento que será enviado na requisição deve conter o pedido. Segue um exemplo: " + 
+			"\n{" + 
+			"\n    \"metodoPagamento\": \"PIX\",\r\n" + //
+			"    \"codigoUsuario\": 1,\r\n" + //
+			"    \"items\": [\r\n" + //
+			"           {\r\n" + //
+			"            \"codigoProduto\": 1,\r\n" + //
+			"            \"quantidade\": 2\r\n" + //
+				 "      }\r\n" + //
+				 "    ],\r\n" + //
+				 "    \"observacao\": \"Cuidado no transporte\"\r\n" + //
+			  "}" +
+			  "\n-- IMPORTANTE -- " + 
+			  "\nESSE PROJETO FUNCIONA EM CONJUNTO COM O MarketplaceRabbitmqApi" +
+			  "\nAMBOS ESTAO CONFIGURADOS PRA RODAR EM PORTAS DIFERENTES" +
+			  "\n------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
+			);
 	}
 
 }
